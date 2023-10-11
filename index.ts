9110118f97e33d9becfb3745f19d87797cfdc537
PM <100664895+OhItsPM@users.noreply.github.com>
2023-10-11 10:46:51 -0700

    added env for templates

--- index.ts
+++ index.ts
@@ -29,33 +29,67 @@ const QUESTIONS = [
         return 'Project name may only include letters, numbers, underscores and hashes.';
     },
   },
+  {
+    name: 'alchemy-api',
+    type: 'input',
+    message: 'Enter your Alchemy API:',
+    validate: function (input: string) {
+      if (/^([A-Za-z\-\\_\d])+$/.test(input)) return true;
+      else return 'API key may only include letters and numbers.';
+    },
+  },
+  {
+    name: 'etherscan-api',
+    type: 'input',
+    message: 'Enter your Etherscan API:',
+    validate: function (input: string) {
+      if (/^([A-Za-z\-\\_\d])+$/.test(input)) return true;
+      else return 'API key may only include letters and numbers.';
+    },
+  },
+  {
+    name: 'walletconnect-id',
+    type: 'input',
+    message: 'Enter your WallectConnect ID:',
+    validate: function (input: string) {
+      if (/^([A-Za-z\-\\_\d])+$/.test(input)) return true;
+      else return 'ID key may only include letters and numbers.';
+    },
+  },
 ];
 
 // Inquirer will ask the user to choose a project template and a project name.
 inquirer.prompt(QUESTIONS).then(answers => {
   const projectChoice = answers['project-template'];
   const projectName = answers['project-name'];
+  const alchemyAPI = answers['alchemy-api'];
+  const etherscanAPI = answers['etherscan-api'];
+  const walletConnectID = answers['walletconnect-id'];
   const templatePath = `${__dirname}/templates/${projectChoice}`;
   const newProjectPath = `${CURR_DIR}/${projectName}`;
   const packageJsonPath = `${templatePath}/package.json`;
+  const envFilePath = `${newProjectPath}/.env`;
+
   console.log('Creating project...');
   // Create the directory for the new project.
   fs.mkdirSync(newProjectPath);
-  console.log('Creating content...');
   // Create the directory contents.
   createDirectorContents(templatePath, projectName);
-
   // Change the current working directory to the project directory.
   process.chdir(newProjectPath);
-  console.log('Updating values...');
   // Read the existing package.json file.
   const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
-
   // Update the 'name' field with the user's project name.
   packageJson.name = projectName;
-
   // Write the updated package.json back to the file.
   fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson));
+
+  // Create the .env file with the environment variables.
+  console.log('Adding environment variables..');
+  fs.writeFileSync(
+    envFilePath,
+    `ALCHEMY_ID=${alchemyAPI}\nETHERSCAN_API=${etherscanAPI}\nWALLET_CONNECT_ID=${walletConnectID}`
+  );
 
   console.log('Project created successfully!');
 });
